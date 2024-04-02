ba568e11b3d507f5a1c0f80df6c4b85505aa4ced
HaelyosStudio <adam.han.martin@gmail.com>
2024-04-02 10:05:13 +0200

    Branch creations

--- App/Views/home.php
+++ App/Views/home.php
@@ -1,8 +1,17 @@
 <?php
 require_once __DIR__ . '/./Includes/header.php';
 ?>
-<main>
-    <h1>Welcome</h1>
-</main>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>BriefQuatuor</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 require_once __DIR__ . '/./Includes/footer.php';
