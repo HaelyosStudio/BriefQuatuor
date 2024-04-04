4a92e23c83fc981a05349b21a535e8517b03059f
Vellanos <davidbak38@gmail.com>
2024-04-04 10:05:09 +0200

    findRole

--- App/Models/Repositories/UserRepository.php
+++ App/Models/Repositories/UserRepository.php
@@ -16,6 +16,19 @@ final class UserRepository extends Database
         parent::__construct();
     }
 
+    public function findRole($userId)
+    {
+        $req = $this->getDb()->prepare('SELECT User.uuid, Role.name FROM User
+        JOIN Role On User.role_id = Role.id
+        WHERE User.uuid = :userId');
+
+        $req->execute([
+            'userId' => $userId
+        ]);
+
+        return $req->fetch();
+    }
+
     // CRUD
     use SQLRequest;
 }
