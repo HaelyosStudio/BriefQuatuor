7f3ea5ae1a484552aea37046e6307c068a75cbcf
Vellanos <davidbak38@gmail.com>
2024-04-04 10:55:34 +0200

    Controller Auth

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -47,6 +47,7 @@ final class AuthController
             if ($userRepo->findOne('User', 'email', $email)) {
                 $user = $userRepo->findOne('User', 'email', $email);
                 $userActive = $user->getActive();
+                $_SESSION['email'] = $email;
                 if ($userActive) {
                     $response = ['email_exists' => true, 'active' => true, 'email' => $email];
                     header('Content-Type: application/json');
@@ -72,10 +73,66 @@ final class AuthController
 
     public function userConfirmPassword()
     {
+        $rawBody = file_get_contents("php://input");
+
+        $body = json_decode($rawBody, true);
+
+        if ($this->issetFormData($body)) {
+            $userRepo = new UserRepository();
+            $email = $_SESSION['email'];
+            $user = $userRepo->findOne('User', 'email', $email);
+            $userId = $user->getUuid();
+            if ($this->checkDoublePassword($body['registrationPassword'], $body['registrationConfirmPassword'])) {
+                $hashedpassword = password_hash($body['registrationPassword'], PASSWORD_DEFAULT);
+                $columnsData = ["password" => $hashedpassword];
+                if ($userRepo->update('User', $columnsData, 'uuid', $userId)) {
+                    $response = ['sucess' => true, 'message' => 'connexion sucess'];
+                    header('Content-Type: application/json');
+                    echo json_encode($response);
+                } else {
+                    $response = ['sucess' => false, 'message' => 'erreur when updating'];
+                    header('Content-Type: application/json');
+                    echo json_encode($response);
+                }
+            } else {
+                $response = ['sucess' => false, 'message' => 'password not identical'];
+                header('Content-Type: application/json');
+                echo json_encode($response);
+            }
+        } else {
+            $response = ['sucess' => false, 'message' => 'Clé(s) manquante(s) dans la requête'];
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }
     }
 
     public function userLogin()
     {
+        $rawBody = file_get_contents("php://input");
+
+        $body = json_decode($rawBody, true);
+
+        if ($this->issetFormData($body)) {
+            $userRepo = new UserRepository();
+            $email = $_SESSION['email'];
+            $passwordInput = $body['connectPassword'];
+            $user = $userRepo->findOne('User', 'email', $email);
+            $userPassword = $user->getPassword();
+            if (password_verify($passwordInput, $userPassword)) {
+                $response = ['sucess' => true, 'message' => 'connexion sucess'];
+                $_SESSION['authenticate_user'] = true;
+                header('Content-Type: application/json');
+                echo json_encode($response);
+            } else {
+                $response = ['sucess' => false, 'message' => 'connexion error'];
+                header('Content-Type: application/json');
+                echo json_encode($response);
+            }
+        } else {
+            $response = ['sucess' => false, 'message' => 'Clé(s) manquante(s) dans la requête'];
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }
     }
 
     public function logout()
